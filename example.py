7ee808213004941bda3b84bc96ecd1c029e7fe80
Bheesetti Harsith Veera Charan <veeracharan99@gmail.com>
2026-02-09 23:31:21 +0530

    while loop python code

--- example.py
+++ example.py
@@ -2,4 +2,4 @@ print("Hello world")
 a=[1,2,3,4,5]
 for i in range(10):
     print(i)
-print("Done")
+print("For loop")
---
+++ file.py
@@ -0,0 +1,6 @@
+print("Hello world")
+i=0
+while(i<10):
+    print(i)
+    i+=1
+print("while loop")
